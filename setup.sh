c474772aa34f5af63a8fcfad7b161249f36bc27e
root <root@kubernetes-demo.maskedadmins.com>
2016-12-23 13:50:51 -0500

    adding docker-distribution

--- setup.sh
+++ setup.sh
@@ -7,9 +7,14 @@ function msg {
   sleep 3
 }
 
-msg Disabling firewalld
+msg Disabling firewalld, adding a rule for the docker registry
 systemctl disable firewalld
 systemctl stop firewalld
+# to do, convert to selinux
+iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 5000 -j ACCEPT
+echo 'iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 5000 -j ACCEPT' >> /etc/rc.d/rc.local
+chmod +x /etc/rc.d/rc.local
+
 
 msg Subscribing to rhn (optional, just echoing commands)
 echo subscription-manager register --serverurl=https://subscription.rhn.redhat.com --auto-attach
@@ -22,9 +27,9 @@ echo subscription-manager repos --enable=rhel-7-server-optional-rpms
 msg running yum repolist. Check if this is ok, then press enter to continue, otherwise Ctrl + C to stop and fix
 yum repolist
 
-msg Installing docker, kubernetes, and etcd
+msg Installing docker and a registry, kubernetes, and etcd
 
-yum -y install kubernetes docker etcd docker-selinux
+yum -y install kubernetes docker etcd docker-selinux docker-distribution
 
 msg Diff of etcd/etcd.conf to be copied over
 diff etcd/etcd.conf /etc/etcd/etcd.conf
@@ -37,8 +42,7 @@ diff kubernetes/kubelet /etc/kubernetes/kubelet
 echo diff kubernetes/apiserver /etc/kubernetes/apiserver
 diff kubernetes/apiserver /etc/kubernetes/apiserver
 
-msg Change the IP addresses to match your setup.
-msg Ctrl + C now to end, or press enter to continue
+msg Change the IP addresses to match your setup.\nCtrl + C now to end, or press enter to continue
 read answer
 
 rsync -avz etcd/ /etc/etcd/
--- start-kubernetes.sh
+++ start-kubernetes.sh
@@ -1,2 +1,2 @@
-for SERVICES in docker etcd kube-proxy kubelet; do     systemctl restart $SERVICES;     systemctl enable $SERVICES;     systemctl is-active $SERVICES; done
-for SERVICES in docker kube-proxy.service kubelet.service; do     systemctl restart $SERVICES;     systemctl enable $SERVICES;     systemctl status $SERVICES; done
+for SERVICES in docker docker-distribution etcd kube-proxy kubelet; do     systemctl restart $SERVICES;     systemctl enable $SERVICES;     systemctl is-active $SERVICES; done
+for SERVICES in docker docker-distribution kube-proxy.service kubelet.service; do     systemctl restart $SERVICES;     systemctl enable $SERVICES;     systemctl status $SERVICES; done
