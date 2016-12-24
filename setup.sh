1bdcbc24187e9c0a9fe0df142628fa9a8973d01f
root <root@kubernetes-demo.maskedadmins.com>
2016-12-24 05:49:41 -0500

    updaing startup scripts

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ echo 'iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 5000 -j ACCEP
 chmod +x /etc/rc.d/rc.local
 
 
-msg Subscribing to rhn (optional, just echoing commands)
+echo msg Subscribing to rhn (optional, just echoing commands)
 echo subscription-manager register --serverurl=https://subscription.rhn.redhat.com --auto-attach
 echo subscription-manager attach
 echo subscription-manager repos --disable=*
@@ -24,17 +24,21 @@ echo subscription-manager repos --enable=rhel-7-server-rpms
 echo subscription-manager repos --enable=rhel-7-server-extras-rpms
 echo subscription-manager repos --enable=rhel-7-server-optional-rpms
 
-msg running yum repolist. Check if this is ok, then press enter to continue, otherwise Ctrl + C to stop and fix
-yum repolist
+echo msg running yum repolist. Check if this is ok, then press enter to continue, otherwise Ctrl + C to stop and fix
+echo yum repolist
 
 msg Installing docker and a registry, kubernetes, and etcd
 
 yum -y install kubernetes docker etcd docker-selinux docker-distribution
 
 msg Diff of etcd/etcd.conf to be copied over
+newip=$(ip addr list eth0 | grep 'inet ' | cut -d ' ' -f 6 | cut -d / -f1)
+sed -i -e "s/XXIPADDRESSXX/${newip}/g" etcd/*
+echo diff etcd/etcd.conf /etc/etcd/etcd.conf
 diff etcd/etcd.conf /etc/etcd/etcd.conf
 
 msg Diff of etc/kubernetes config, kubelet, and apiserver files to be copied over
+sed -i -e "s/XXIPADDRESSXX/${newip}/g" kubernetes/*
 echo diff kubernetes/config /etc/kubernetes/config
 diff kubernetes/config /etc/kubernetes/config
 echo diff kubernetes/kubelet /etc/kubernetes/kubelet
@@ -42,7 +46,8 @@ diff kubernetes/kubelet /etc/kubernetes/kubelet
 echo diff kubernetes/apiserver /etc/kubernetes/apiserver
 diff kubernetes/apiserver /etc/kubernetes/apiserver
 
-msg Change the IP addresses to match your setup.\nCtrl + C now to end, or press enter to continue
+msg Check if the IP addresses correct for your setup. \
+Ctrl + C now to end, or press enter to continue
 read answer
 
 rsync -avz etcd/ /etc/etcd/
@@ -51,4 +56,4 @@ rsync -avz kubernetes/ /etc/kubernetes/
 msg Enabling and starting services
 ./start-kubernetes.sh
 
-msg Try 'kubectl get pods' or 'kubectl -s 10.55.2.251 get pods'
+msg Try 'kubectl get pods' or 'kubectl -s ${newip} get pods'
--- start-kubernetes.sh
+++ start-kubernetes.sh
@@ -1,2 +1,4 @@
-for SERVICES in docker docker-distribution etcd kube-proxy kubelet; do     systemctl restart $SERVICES;     systemctl enable $SERVICES;     systemctl is-active $SERVICES; done
-for SERVICES in docker docker-distribution kube-proxy.service kubelet.service; do     systemctl restart $SERVICES;     systemctl enable $SERVICES;     systemctl status $SERVICES; done
+for SERVICES in docker docker-distribution etcd kube-apiserver kube-controller-manager kubelet kube-proxy.service kube-scheduler.service
+do
+  systemctl restart $SERVICES;     systemctl enable $SERVICES;     systemctl is-active $SERVICES; systemctl status $SERVICES
+done
